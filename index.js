583601aa1dcac5ddecf0977a2ff4116c243e69af
Ryba <julorybarczyk@gmail.com>
2022-12-22 20:28:51 +0100

    No tak, zapomniałem o CORSie -_-

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json())
+const cors = require('cors');
+const corsOptions ={
+    origin:'*',
+    credentials:true,
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 app.get('/plan', async (req, res)=>{
     res.status(200).send(
